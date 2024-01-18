f2697da2c3979bee1bef8d060d08c631c9fdc297
SAURABH-KHARTODE123 <saurabhkhartode73@gmail.com>
2024-01-18 13:30:10 +0530

    JWTToken

---
+++ SpringbootJWTToken/src/main/java/com/demo/SpringbootJWTToken.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJWTToken {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJWTToken.class,args);
+
+	}
+
+}
